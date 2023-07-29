7e3ab39949cbe4f5741e41b29101344fc5ed6184
Harobulus <harobulus@gmail.com>
2023-07-29 17:54:17 +0100

    Created the code to display pet objects

--- js/script.js
+++ js/script.js
@@ -35,6 +35,31 @@ const francine = createPet("Francine", "turtle");
 createPet(clover.sleep());
 createPet(baxter.play());
 
-console.log(clover);
-console.log(baxter);
 
+console.log(baxter);
+console.log(clover);
+// Change the value of the isTired property to 8 for Clover. 
+// Also, change the value of the isTired property to 9 for Francine.
+clover.isTired = 8;
+francine.isTired = 9;
+
+// Create an array called allPets that includes all pet objects as elements.
+const allPets = [sora, clover, baxter, cleo, francine];
+
+// Log out the allPets array to verify all five objects are showing up as array elements.
+console.log(allPets);
+
+// Create a function called showPets. Use petArray as an argument.
+const showPets = function (petArray) {
+    pets.innerHTML = "";
+    for (let pet of petArray) {
+        let status = "ready to play!";
+        if (pet.isTired > 7) {
+            status = "sleeping";
+        };
+        const li = document.createElement("li");
+        li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+        pets.append(li);
+    }
+};
+statusButton.addEventListener("click", function () {showPets (allPets)});
